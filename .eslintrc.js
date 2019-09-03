5c0f7216c3fa1875785ca7fd322d36359e6ed3cf
chen_anguo <cag2050@163.com>
2019-09-03 17:53:16 +0800

    缩进使用4个空格

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "@vue/prettier"],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        indent: ["error", 4, { SwitchCase: 1 }] // 缩进宽度4个空格
+    }
+}
